48f724a361332aa6c5417ef458af074679ecf006
zouqingsong <zouqingsong@gmail.com>
2025-08-09 21:00:42 +0800

    Update camera_controller.dart

--- packages/camera/camera/lib/src/camera_controller.dart
+++ packages/camera/camera/lib/src/camera_controller.dart
@@ -222,6 +222,7 @@ class CameraValue {
         'flashMode: $flashMode, '
         'exposureMode: $exposureMode, '
         'focusMode: $focusMode, '
+        'whiteBalanceMode: $whiteBalanceMode, '
         'exposurePointSupported: $exposurePointSupported, '
         'focusPointSupported: $focusPointSupported, '
         'deviceOrientation: $deviceOrientation, '
